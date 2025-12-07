48ff48ea10dbbf331aca53e315e856c0940ee930
Francis Ferrando <francisferrando@hotmail.com>
2025-12-07 14:21:41 -0300

    Se agregaron las migraciones para reflejar los cambios en las entidades para soportar alquileres

---
+++ BibliotecaDevlights.Data/Migrations/20251207172020_AddedRental.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BibliotecaDevlights.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRental : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReturned",
+                table: "OrderItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RentalEndDate",
+                table: "OrderItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RentalReturnedDate",
+                table: "OrderItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RentalStartDate",
+                table: "OrderItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "OrderItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RentalEndDate",
+                table: "CartItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RentalStartDate",
+                table: "CartItems",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReturned",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "RentalEndDate",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "RentalReturnedDate",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "RentalStartDate",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "RentalEndDate",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "RentalStartDate",
+                table: "CartItems");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "CartItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
